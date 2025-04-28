a5666f434c322ef25683552e67233cfb87f19370
= <ggrerqq2346@rambler.ru>
2025-04-28 21:53:56 +0300

    fix

--- tests/lvl/lvl_tests.py
+++ tests/lvl/lvl_tests.py
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
-import os
-import sys
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..')))
-=======
 from os import system
->>>>>>> debug
 
 import unittest
 
